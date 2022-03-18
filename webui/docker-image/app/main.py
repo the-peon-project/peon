cfea3c4fd43839ecad051ec77b1108e43bca459c
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-18 17:53:36 +0000

    Added logging

--- webui/docker-image/app/main.py
+++ webui/docker-image/app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import logging
 
 # the all-important app variable:
 app = Flask(__name__)
@@ -8,4 +9,5 @@ def hello():
     return "Welcome to PEON. The game server manager."
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='/var/log/peon/webui.log', filemode='a', format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
     app.run(host='0.0.0.0', debug=True, port=80)
