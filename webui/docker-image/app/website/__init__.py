94fc378eaad0f5bc1993996a1e64d93ec60872b6
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-18 18:47:20 +0000

    Removed other junk

--- webui/docker-image/app/website/__init__.py
+++ webui/docker-image/app/website/__init__.py
@@ -11,36 +11,5 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 
 def create_app():
-    settings = json.load(open("../config/settings.json".format(rootpath), 'r'))
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'thedefaultpeonsecretkey'
-    # Initialise  DB
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{0}:{1}@{2}:5432/{3}".format(settings["db"]["username"],settings["db"]["password"],settings["db"]["hostname"],settings["db"]["database"])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    migrate = Migrate(app, db)
-    from .models import User,Permissions
-    # create_database(app)
-    logging.debug ("----> [ WebApp ] Starting...")
-
-    # URL Handlers
-    #  Import url handlers
-    from .views import views
-    from .auth import auth
-    from .opps import opps
-    from .finance import finance
-    #  Register url handlers
-    app.register_blueprint(views, url_prefix='/')  
-    app.register_blueprint(auth, url_prefix='/')  
-    app.register_blueprint(opps, url_prefix='/')  
-    app.register_blueprint(finance, url_prefix='/')  
-
-    # Login Manager
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
     return app
