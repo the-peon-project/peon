80c42d355e6b6f94568ed197462695146431a08b
Richard Raymond <richard.raymond@datasciences.co.za>
2022-03-18 16:02:54 +0200

    Change temp splash

--- webui/docker-image/app/main.py
+++ webui/docker-image/app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Oh, Hello World"
+    return "Welcome to PEON. The game server manager."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
