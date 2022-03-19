b9a126b1532372cc63f6802232a1210e4eab518f
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-19 19:45:13 +0000

    First draft API

--- docker-images/orchestrator/app/app.py
+++ docker-images/orchestrator/app/app.py
@@ -5,115 +5,98 @@ from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 app = Flask(__name__)
 api = Api(app)
 
-# A List of Dicts to store all of the books
-books = [{
-    "id": 1,
-    "title": "Zero to One",
-    "author": "Peter Thiel",
-    "length": 195,
-    "rating": 4.17
-},
-    {
-    "id": 2,
-    "title": "Atomic Habits ",
-    "author": "James Clear",
-    "length": 319,
-    "rating": 4.35
-}
-]
+# A List of Dicts to store all of the servers
+servers = []
 
-# Schema For the Book Request JSON
-bookFields = {
+# Schema For the Server Request JSON
+serverFields = {
     "id": fields.Integer,
-    "title": fields.String,
-    "author": fields.String,
-    "length": fields.Integer,
-    "rating": fields.Float
+    "game_id": fields.String,
+    "servername": fields.String,
+    "password": fields.String,
+    "state": fields.String
 }
 
-
-# Resource: Individual Book Routes
-class Book(Resource):
+# Resource: Individual Server Routes
+class Server(Resource):
     def __init__(self):
-        # Initialize The Flsak Request Parser and add arguments as in an expected request
+        # Initialize The Flask Request Parser and add arguments as in an expected request
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument("title", type=str, location="json")
-        self.reqparse.add_argument("author", type=str, location="json")
-        self.reqparse.add_argument("length", type=int, location="json")
-        self.reqparse.add_argument("rating", type=float, location="json")
+        self.reqparse.add_argument("game_id", type=str, location="json")
+        self.reqparse.add_argument("servername", type=str, location="json")
+        self.reqparse.add_argument("password", type=str, location="json")
+        self.reqparse.add_argument("state", type=str, location="json")
 
-        super(Book, self).__init__()
+        super(Server, self).__init__()
 
-    # GET - Returns a single book object given a matching id
+    # GET - Returns a single server object given a matching id
     def get(self, id):
-        book = [book for book in books if book['id'] == id]
+        server = [server for server in servers if server['id'] == id]
 
-        if(len(book) == 0):
+        if(len(server) == 0):
             abort(404)
 
-        return{"book": marshal(book[0], bookFields)}
+        return{"server": marshal(server[0], serverFields)}
 
     # PUT - Given an id
     def put(self, id):
-        book = [book for book in books if book['id'] == id]
+        server = [server for server in servers if server['id'] == id]
 
-        if len(book) == 0:
+        if len(server) == 0:
             abort(404)
 
-        book = book[0]
+        server = server[0]
 
         # Loop Through all the passed agruments
         args = self.reqparse.parse_args()
         for k, v in args.items():
             # Check if the passed value is not null
             if v is not None:
-                # if not, set the element in the books dict with the 'k' object to the value provided in the request.
-                book[k] = v
+                # if not, set the element in the servers dict with the 'k' object to the value provided in the request.
+                server[k] = v
 
-        return{"book": marshal(book, bookFields)}
+        return{"server": marshal(server, serverFields)}
 
     def delete(self, id):
-        book = [book for book in books if book['id'] == id]
+        server = [server for server in servers if server['id'] == id]
 
-        if(len(book) == 0):
+        if(len(server) == 0):
             abort(404)
 
-        books.remove(book[0])
+        servers.remove(server[0])
 
         return 201
 
 
-class BookList(Resource):
+class ServerList(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument(
-            "title", type=str, required=True, help="The title of the book must be provided", location="json")
+            "game_id", type=str, required=True, help="The PEON game id must be provided", location="json")
         self.reqparse.add_argument(
-            "author", type=str, required=True, help="The author of the book must be provided", location="json")
-        self.reqparse.add_argument("length", type=int, required=True,
-                                   help="The length of the book (in pages)", location="json")
+            "servername", type=str, required=True, help="A custom server name must be provided", location="json")
         self.reqparse.add_argument(
-            "rating", type=float, required=True, help="The rating must be provided", location="json")
+            "password", type=str, required=True, help="A custom server password must be provided", location="json")
 
     def get(self):
-        return{"books": [marshal(book, bookFields) for book in books]}
+        return{"servers": [marshal(server, serverFields) for server in servers]}
 
     def post(self):
         args = self.reqparse.parse_args()
-        book = {
-            "id": books[-1]['id'] + 1 if len(books) > 0 else 1,
-            "title": args["title"],
-            "author": args["author"],
-            "length": args["length"],
-            "rating": args["rating"]
+        server = {
+            "id": servers[-1]['id'] + 1 if len(servers) > 0 else 1,
+            "game_id": args["game_id"],
+            "servername": args["servername"],
+            "password": args["password"]
         }
 
-        books.append(book)
-        return{"book": marshal(book, bookFields)}, 201
+        servers.append(server)
+        return{"server": marshal(server, serverFields)}, 201
 
 
-api.add_resource(BookList, "/books")
-api.add_resource(Book, "/books/<int:id>")
+api.add_resource(ServerList, "/servers")
+api.add_resource(Server, "/servers/<int:id>")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
