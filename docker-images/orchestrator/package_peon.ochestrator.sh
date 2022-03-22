d65069d6b9743248942fb62ca16e3cb2fe2e65f3
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-22 20:52:57 +0000

    Renamed package

--- docker-images/orchestrator/package_peon.ochestrator.sh
+++ docker-images/orchestrator/package_peon.ochestrator.sh
@@ -2,7 +2,7 @@
 version=$1
 if [[ $version ]];
 then
-    docker build -t umlatt/peon.orchestrator:$version .
+    docker build -t umlatt/peon.orc:$version .
 else
     printf "Version number required.\ne.g.\t$0 1.0.2\n"
 fi
