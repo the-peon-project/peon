2eda8faec75c22377fdac25d8abe11b88c17fd1c
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-22 19:33:24 +0000

    Reworked logging

--- recipies/csgo/run_steamcmd.sh
+++ recipies/csgo/run_steamcmd.sh
@@ -4,10 +4,14 @@ game_id='740'
 #SCRIPT START
 script="${0##*/}"
 logfile="/var/log/peon/$script.log"
-echo "Updating steamcmd." >> $logfile
-./steamcmd.sh +app_update +quit >> $logfile
-echo "Installing/configuring the game server." >> $logfile
-./steamcmd.sh +force_install_dir /data +login anonymous +app_update $game_id +quit >> $logfile
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>$logfile 2>&1
+
+echo "Updating steamcmd."
+stdbuf -oL ./steamcmd.sh +app_update +quit
+echo "Installing/configuring the game server."
+stdbuf -oL ./steamcmd.sh +force_install_dir ./data +login anonymous +app_update $game_id +quit
 echo "Adding server ready file, for hand back"
 echo "READY" > ./data/server.state
-echo "Processing complete." >> $logfile
+echo "Processing complete."
