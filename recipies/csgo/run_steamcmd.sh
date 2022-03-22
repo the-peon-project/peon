df4c6e30700b4218bdc9d1095a63c89f76929da9
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-22 15:13:14 +0000

    Added steam game id variable

--- recipies/csgo/run_steamcmd.sh
+++ recipies/csgo/run_steamcmd.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# STEAM GAME ID
+game_id='740'
+#SCRIPT START
 script="${0##*/}"
 logfile="/var/log/peon/$script.log"
 echo "Updating steamcmd." >> $logfile
 ./steamcmd.sh +app_update +quit >> $logfile
 echo "Installing/configuring the game server." >> $logfile
-./steamcmd.sh +force_install_dir /data +login anonymous +app_update 740 +quit >> $logfile
+./steamcmd.sh +force_install_dir /data +login anonymous +app_update $game_id +quit >> $logfile
 echo "Adding server ready file, for hand back"
 echo "READY" > ./data/server.state
 echo "Processing complete." >> $logfile
