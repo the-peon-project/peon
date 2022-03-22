17780769421b10be926f24021963e239e4977c07
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-22 14:33:23 +0000

    Added quit after update

--- recipies/csgo/run_steamcmd.sh
+++ recipies/csgo/run_steamcmd.sh
@@ -3,8 +3,8 @@ script="${0##*/}"
 mkdir -p data/docker/logs
 logfile="./data/docker/logs/$script.log"
 echo "Updating steamcmd." >> $logfile
-./steamcmd.sh +app_update
-echo "Installing/configuring the game server."
+./steamcmd.sh +app_update +quit
+echo "Installing/configuring the game server." >> $logfile
 ./steamcmd.sh +login anonymous +force_install_dir /data +app_update 740 +quit >> $logfile
 echo "Adding server ready file, for hand back"
 touch ./data/server.ready
