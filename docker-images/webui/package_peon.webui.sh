cac136ace77ce487cda9b2c3a2b74608395ea3a8
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-22 20:53:05 +0000

    Renamed package

--- docker-images/webui/package_peon.webui.sh
+++ docker-images/webui/package_peon.webui.sh
@@ -2,7 +2,7 @@
 version=$1
 if [[ $version ]];
 then
-    docker build -t umlatt/peon.webui:$version .
+    docker build -t umlatt/peon.ui:$version .
 else
     printf "Version number required.\ne.g.\t$0 1.0.2\n"
 fi
