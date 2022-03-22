c365558dcbf3cdd094f4617bef28375a9e605d07
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-22 15:11:29 +0000

    Made server.state file dynamic

--- recipies/csgo/run_steamcmd.sh
+++ recipies/csgo/run_steamcmd.sh
@@ -6,5 +6,5 @@ echo "Updating steamcmd." >> $logfile
 echo "Installing/configuring the game server." >> $logfile
 ./steamcmd.sh +force_install_dir /data +login anonymous +app_update 740 +quit >> $logfile
 echo "Adding server ready file, for hand back"
-touch ./data/server.ready
+echo "READY" > ./data/server.state
 echo "Processing complete." >> $logfile
