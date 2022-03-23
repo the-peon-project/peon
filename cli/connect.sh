05c62727dbd92ca91ea43f5282ed6186ac93a2b1
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-23 19:58:36 +0000

    Made connector only work for game servers

--- cli/connect.sh
+++ cli/connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PS3="Please select a container to enter: "
-container_list=`docker ps --format "{{.Names}}" | grep -i 'peon'`
+container_list=`docker ps --format "{{.Names}}" | grep -i 'peon.warcamp'`
 select container in $container_list; do
     if [ $REPLY -gt 0 ]; then
         echo -e "Connecting to [\033[0;34m$container\033[0m]"
