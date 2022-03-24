6f2912f8893be6fd85329f358173023fbba2a672
Umlatt <theplacetosendmemail@gmail.com>
2022-03-24 09:30:42 +0000

    Added menu_draw v1

--- cli-v2/shared.sh
+++ cli-v2/shared.sh
@@ -11,6 +11,21 @@ pause() {
     read -p "Press [Enter] key to continue..." fackEnterKey
 }
 
+draw_menu_header() {
+    width=$1 title=$2 heading=$3 bar_heavy="" bar_light="" even_space=""
+    title_size=${#title} heading_size=${#heading}
+    if ! (( $width % 2 )) ; then even_space=" "; fi
+    for ((i = 1; i <= $width; i++)); do bar_heavy+="━"; done
+    for ((i = 1; i <= $(($width - 4)); i++)); do bar_light+="─"; done
+    mid_bar=$(sed -e "s/./┯/2" <<<$bar_heavy)
+    mid_bar=$(sed -e "s/./┯/$(($width - 1))" <<<$mid_bar)
+    title_gap=$(printf "%*s%s" $((($width - $title_size) / 2)) '' "$line")
+    heading_gap=$(printf "%*s%s" $((($width - $heading_size - 4) / 2)) '' "$line")
+    printf "┏$bar_heavy┓\n┃$title_gap$title$title_gap$even_space┃\n┗$mid_bar┛\n"
+    printf "  │$heading_gap$heading$heading_gap$even_space│  \n  └$bar_light┘\n"
+    pause
+}
+
 list_all_containers() {
     clear
     printf "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n"
@@ -20,9 +35,4 @@ list_all_containers() {
     printf "  └───────────────────────────────┘\n"
     docker ps -a --format "table {{.Names}}\t{{.State}}"
     pause
-}
-
-# do something in two()
-two() {
-    printf "two() called"
 }
