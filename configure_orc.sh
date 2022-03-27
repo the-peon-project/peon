66d94b887bf95a95a15eecfcf8725330fa1e0c9e
Umlatt <theplacetosendmemail@gmail.com>
2022-03-27 20:00:14 +0000

    Added send docker cli client to orc

--- configure_orc.sh
+++ configure_orc.sh
@@ -6,3 +6,7 @@
 docker exec -it peon.orc ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
 # Update authorized keys on docker host
 docker exec -it peon.orc cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+# Copy docker cli binary into container - MAY NOT BE BEST WAY (different OS's/builds)
+
+# Copy over current docker binary (here to ignore architecture)
+docker cp /usr/bin/docker peon.orc:/usr/bin/.
