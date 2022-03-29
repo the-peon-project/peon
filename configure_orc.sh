240501947d77f3e789c8e6d8d19da307f5a5dd74
Umlatt <theplacetosendmemail@gmail.com>
2022-03-29 19:34:30 +0000

    Added authorisation

--- configure_orc.sh
+++ configure_orc.sh
@@ -5,8 +5,8 @@
 # Create ssh key in orchestrator
 docker exec -it peon.orc ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
 # Update authorized keys on docker host
-docker exec -it peon.orc cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+docker exec -it peon.orc bash -c "cat /root/.ssh/id_rsa.pub" >> /root/.ssh/authorized_keys
+# Preload ECDSA key
+docker exec -it peon.orc bash -c "ssh-keyscan -p 22222 -t rsa 172.20.0.1 | sed -E 's/^([^ \[]+) (.*)$/[\1]:NNN \2/' >> ~/.ssh/known_hosts"
 # Copy docker cli binary into container - MAY NOT BE BEST WAY (different OS's/builds)
-
-# Copy over current docker binary (here to ignore architecture)
 docker cp /usr/bin/docker peon.orc:/usr/bin/.
