95f3212e40815f8e16411060ec140276f02a7e54
umlatt <theplacetosendmemail@gmail.com>
2023-04-12 20:29:27 +0000

    BugFix - remove orphans

--- deploy_peon.sh
+++ deploy_peon.sh
@@ -6,4 +6,4 @@ cat "$config_path/02_orc.yml"       >> docker-compose.yml
 cat "$config_path/03_webui.yml"     >> docker-compose.yml
 cat "$config_path/05_docs.yml"      >> docker-compose.yml
 cat "$config_path/20_bots.yml"      >> docker-compose.yml
-docker-compose up -d
+docker-compose up --remove-orphans -d
