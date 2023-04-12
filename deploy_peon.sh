9c773661c945d699fe59cd4b3e80ced022047af5
umlatt <theplacetosendmemail@gmail.com>
2023-04-12 20:21:51 +0000

    Moved to dynamic env builder

---
+++ deploy_peon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+config_path="./config/docker-compose"
+cat "$config_path/01_network.yml"   > docker-compose.yml
+cat "$config_path/02_orc.yml"       >> docker-compose.yml
+cat "$config_path/03_webui.yml"     >> docker-compose.yml
+cat "$config_path/05_docs.yml"      >> docker-compose.yml
+cat "$config_path/20_bots.yml"      >> docker-compose.yml
+docker-compose up -d
