889fbe8825ca728b787a6430ecb18a42c371d08d
umlatt <theplacetosendmemail@gmail.com>
2023-04-13 20:04:56 +0000

    Improved visual layout

--- documentation/diagram_definitions/comms_orchestrator.py
+++ documentation/diagram_definitions/comms_orchestrator.py
@@ -4,21 +4,25 @@ from diagrams.onprem.container import Docker
 from diagrams.onprem.client import User, Client
 from diagrams.saas.chat import Discord
 from diagrams.oci.compute import OKE
+from diagrams.custom import Custom
 
 comms_http = "darkgreen"
 comms_container_service = "darkblue"
 comms_container = "darkblue"
 
-with Diagram("Peon Orchestrator", filename="../documentation/manual/docs/architecture/comms_orchestrator", show=False):
+with Diagram("Peon Orchestrator Flow", filename="../documentation/manual/docs/architecture/comms_orchestrator", show=False):
     # Nodes
-    client_user = User("User")
-    client_bot_discord = Discord("Discord Bot*")
-    peon_web = Client("PEON WebApp*")
+    user = User("User")
+    client_rest = Client("REST Client")
+    peon_web = Docker("PEON WebApp*")
+    bot_discord = Docker("PEON Discord Bot*")
+    client_discord = Discord("Discord")
+    client_web = Client("WebUI Portal")
     # Clusters
     with Cluster("Container Host"):
         # Cluster - Nodes
         server = OKE("Container Service")
-        container_orc = Docker("Orchestrator")
+        container_orc = Docker("PEON Orchestrator")
         servers = [
             Docker("Game Server N"), 
             Docker("Game Server 02"), 
@@ -26,8 +30,13 @@ with Diagram("Peon Orchestrator", filename="../documentation/manual/docs/archite
         # Cluster - Connectivity
         container_orc >> Edge(label="docker.sock",color=comms_container_service) << server
         server - Edge(color=comms_container, style="dotted") - servers
-
     # Connectivity
-    client_user >> Edge(label="REST API", color=comms_http) << container_orc
-    client_bot_discord >> Edge(label="REST API", color=comms_http) << container_orc
-    peon_web >> Edge(label="REST API", color=comms_http) << container_orc
+    client_rest >> Edge(color=comms_http) << container_orc
+    # Web
+    client_web - peon_web
+    peon_web >> Edge(color=comms_http) << container_orc
+    # Discord
+    client_discord - bot_discord
+    bot_discord >> Edge(color=comms_http) << container_orc
+    # User
+    user >> [client_rest, client_discord, client_web]
