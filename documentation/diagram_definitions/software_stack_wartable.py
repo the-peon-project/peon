bc41f25b61db1e7a76b36cb1cfb97a16c00816db
umlatt <theplacetosendmemail@gmail.com>
2023-04-14 20:11:16 +0000

    Initial commit

---
+++ documentation/diagram_definitions/software_stack_wartable.py
@@ -0,0 +1,23 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.programming.language import Bash
+from diagrams.onprem.container import Docker
+from diagrams.custom import Custom
+import os
+
+# COLOURS
+comms_http = "orange"
+comms_container_service = "red"
+comms_container = "darkgray"
+
+current_path = os.getcwd()
+
+
+with Diagram("Peon War Table Software Stack", filename="../documentation/manual/docs/development/diagram_wartable", show=False):
+    with Cluster("Container"):
+        with Cluster("Application"):
+            wine = Custom("WINE",f"{current_path}/diagram_definitions/logos/wine.png")
+            with Cluster("Base Image"):
+                steam = Custom("SteamCMD",f"{current_path}/diagram_definitions/logos/steam.png")
+            #wine  - Edge(color=comms_container, style="dotted") - steam
+        Docker("umlatt/steamcmd-winehq")
+    Bash("Automation Scripts")
